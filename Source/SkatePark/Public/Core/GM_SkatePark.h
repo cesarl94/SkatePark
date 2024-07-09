afb9c49fb64fe50d10e1c08a5793a0abf5282895
Cesar Lorenzon <lorenzon.cesar@hotmail.com>
2024-07-09 14:27:34 -0300

    Organized assets and created base classess

---
+++ Source/SkatePark/Public/Core/GM_SkatePark.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GM_SkatePark.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKATEPARK_API AGM_SkatePark : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
